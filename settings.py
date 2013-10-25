ca4599e3d1aa2d3827fd08329fad7011805fbd4f
xiaxiaoyu1988 <xyyvsxh@gmail.com>
2013-10-25 22:59:42 +0800

    Update header for diy

--- settings.py
+++ settings.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python27
+#-*- coding:utf-8 -*-
+
 class Settings:
-	site_name = "blogpy"
-	site_root = "http://blogpy.reddino.org/"
-	site_description = "A simple static blog generator in python"
+	site_name = "Xiaxiaoyu's github"
+	site_root = "http://xiaxiaoyu1988.github.io/"
+	site_description = u"边走边悟"
 	number_of_posts_on_front_page = 5

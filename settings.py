5b3e1593344e58c2a869ea1a192e93a53df9ee99
xiaxiaoyu1988 <xyyvsxh@gmail.com>
2013-10-27 19:56:46 +0800

    update setting.py for missing '/'

--- settings.py
+++ settings.py
@@ -3,6 +3,6 @@
 
 class Settings:
 	site_name = "Xiaxiaoyu's GitHub"
-	site_root = "http://git.xtimer.org"
+	site_root = "http://git.xtimer.org/"
 	site_description = u"边走边悟"
 	number_of_posts_on_front_page = 5

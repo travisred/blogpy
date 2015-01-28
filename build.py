b5ae5645707004ebd120599455c6ad8b16e781a1
Travis Reddell <tjreddell@gmail.com>
2015-01-28 10:03:57 -0700

    clean up build script, remove required html from markdown template

--- build.py
+++ build.py
@@ -1,144 +1,104 @@
-import os, markdown, codecs, string, re
+import os, markdown, codecs, re
 from settings import Settings
+class Post:
+    title = ""
+    date = ""
+    url = ""
 
-index_posts = []
-recent_posts = []
-archive_links = []
-
-posts = os.listdir(os.getcwd()+"/md")
-index_file = codecs.open("site/index.html", mode="w", encoding="utf8")
-rss_file = codecs.open("site/rss.xml", mode="w", encoding="utf8")
-archive_file = codecs.open("site/archive/index.html", mode="w", encoding="utf8")
-
-header_file = codecs.open("static/header", mode="r", encoding="utf8")
-header = header_file.read()
-header_file.close()
-header = string.replace(header, "site_root", Settings.site_root)
-header = string.replace(header, "site_name", Settings.site_name)
-header = string.replace(header, "site_description", Settings.site_description)
-
-footer_file = codecs.open("static/footer", mode="r", encoding="utf8")
-footer = footer_file.read()
-footer_file.close()
-footer = string.replace(footer, "site_root", Settings.site_root)
-
-# The ugliest RSS feed generator. Should clean it up someday.
-def rss(posts, rss_file):
+def rss(posts):
   rss_body = """<rss version="2.0">
   <channel>
   \t<title>""" + Settings.site_name + """</title>
   \t<link>""" + Settings.site_root + """</link>
   \t<description>""" + Settings.site_description + """</description>"""
-  posts_dates = []
+  
   for post in posts:
-    input_file = codecs.open("md/"+post, mode="r", encoding="utf8")
-    file_content = input_file.read()
-    file_content_lines = string.split(file_content, '\n')
-    title = string.replace(file_content_lines[0], '# ', '')
-    title = string.replace(title, ' #', '')
-    date = file_content_lines[2]
-    input_file.close()
-    posts_dates.append(date + "**" + title + "**" + post) #ermygawd, custom delimiter!
-    posts_dates.sort()
-    posts_dates.reverse()
-
-  for p in posts_dates:
-    post_pieces = string.split(p, "**")
-    rss_body += """\n\t\t<item>
-    \t<title>""" + post_pieces[1] + """</title>
-    \t<pubDate>""" + post_pieces[0] + """</pubDate>
-    \t<link>""" + Settings.site_root + post_pieces[2] + """/</link>
-    </item>"""
+    rss_body += """\n\t<item>
+    \t\t<title>""" + post.title + """</title>
+    \t\t<pubDate>""" + post.date + """</pubDate>
+    \t\t<link>""" + post.url + """</link>
+    \t</item>"""
 
   rss_body += "\n\t</channel>\n</rss>"
+  rss_file = codecs.open("site/rss.xml", mode="w", encoding="utf8")
   rss_file.write(rss_body)
   rss_file.close()
 
+def parse_posts(posts):
+  parsed_posts = []
+  for post in posts:
+    parsed_post = Post()
 
-for post in posts:
-  input_file = codecs.open("md/"+post, mode="r", encoding="utf8")
+    # Read post markdown file
+    input_file = codecs.open("md/"+post, mode="r", encoding="utf8")
+    file_content = input_file.read()
+    input_file.close()
 
-  file_content = input_file.read()
-  file_content_lines = string.split(file_content, '\n')
-  title = file_content_lines[0]
-  date = file_content_lines[2]
-  input_file.close()
+    file_content_lines = file_content.split('\n')
+    
+    parsed_post.slug = post
+    parsed_post.title = file_content_lines[0]
+    parsed_post.date = file_content_lines[1]
+    parsed_post.url = Settings.site_root + post + '/'
+    parsed_post.html = markdown.Markdown().convert('\n'.join(file_content_lines[2:]))
+    parsed_posts.append(parsed_post)
+      
+    parsed_posts.sort(key = lambda x: x.date, reverse=True)
+  
+  return parsed_posts
 
-  md = markdown.Markdown()
-  html = md.convert(file_content)
+posts = os.listdir(os.getcwd()+"/md")
 
-  html_post_start = re.search('<div class="articleline2"></div>', html)
+header_file = codecs.open("static/header", mode="r", encoding="utf8")
+header = header_file.read()
+header_file.close()
+header = header.replace("site_root", Settings.site_root)
+header = header.replace("site_name", Settings.site_name)
+header = header.replace("site_description", Settings.site_description)
 
-  index_posts.append("<hr><div class="+date+"><a href='"+Settings.site_root+post+"/'><h1>" + string.replace(title, '#', '') + "</h1></a>"+html[html_post_start.start():]+"</div>")
-  archive_links.append("<span>"+date+"</span><a href='"+Settings.site_root+post+"/'>" + string.replace(title, '#', '') + "</a><br>")
-  recent_posts.append("<li class="+date+"><a href='"+Settings.site_root+post+"/'>"+string.replace(title, '#', '') +"</a></li>")
+footer_file = codecs.open("static/footer", mode="r", encoding="utf8")
+footer = footer_file.read()
+footer_file.close()
+footer = footer.replace("site_root", Settings.site_root)
 
-#sort posts by publication date
-recent_posts.sort()
-recent_posts.reverse()
-
-#sort archive links by publication date
-archive_links.sort()
-archive_links.reverse()
-
-#sort index posts by publication date
-index_posts.sort()
-index_posts.reverse()
+parsed_posts = parse_posts(posts)
 
 postgroup = ""
-postgroup_count=0
-for post in recent_posts:
-  if(postgroup_count<12):
-    postgroup = postgroup + post
-  postgroup_count+=1
+for i in range(len(parsed_posts[0:11])):
+  postgroup = postgroup + "<li><a href='" + parsed_posts[i].url + "'>" + parsed_posts[i].title + "</a></li>"
 
 index_body = ""
-index_post_count = 0
-for link in index_posts:
-  if (index_post_count < Settings.number_of_posts_on_front_page):
-   index_body = index_body + link
-  index_post_count += 1
+for i in range(len(parsed_posts[0:Settings.number_of_posts_on_front_page - 1])):
+  index_html = parsed_posts[i].html.replace('../img/', 'img/')
+  index_body = index_body + "<div><a href='" + parsed_posts[i].url + "'><h1>" + parsed_posts[i].title + "</h1><hr>" + parsed_posts[i].date + "</a>" + index_html + "</div>"
 
-index_header = string.replace(header, "page_title", Settings.site_name)
-index_file.write(index_header + index_body + string.replace(footer, "<!-- recent posts -->", postgroup))
+index_header = header.replace("page_title", Settings.site_name)
+index_file = codecs.open("site/index.html", mode="w", encoding="utf8")
+index_file.write(index_header + index_body + footer.replace("<!-- recent posts -->", postgroup))
 index_file.close()
 
-post_count = 0
-for post in posts:
+for parsed_post in parsed_posts:
+  if not os.path.exists("site/" + parsed_post.slug):
+    os.makedirs("site/" + parsed_post.slug)
+  post_file = codecs.open("site/" + parsed_post.slug + "/index.html", "w", encoding="utf8")
 
-  post_count+=1
-
-  input_file = codecs.open("md/"+post, mode="r", encoding="utf8")
-  file_content = input_file.read()
-  input_file.close()
-
-  title = string.split(file_content, '\n')[0]
-  
-  md = markdown.Markdown()
-  html = md.convert(file_content)
-  if not os.path.exists("site/" + post):
-    os.makedirs("site/" + post)
-  post_file = codecs.open("site/"+post+"/index.html", "w", encoding="utf8")
-
-  title = string.replace(title, '#', '')
-  title = string.replace(title, '\n', '')
-  curr_header = string.replace(header, "page_title", title)
-  curr_header = string.replace(curr_header, "css/style.css", "../css/style.css")
-
-  post_file.write(curr_header + html + string.replace(footer, "<!-- recent posts -->", postgroup))
+  curr_header = header.replace("page_title", parsed_post.title)
+  curr_header = curr_header.replace("css/style.css", "../css/style.css")
+  title_h1 = "<h1>" + parsed_post.title + "</h1>"
+  date_hr = "<hr>" + parsed_post.date + "<hr>"
+  post_file.write(curr_header + title_h1 + date_hr + parsed_post.html + footer.replace("<!-- recent posts -->", postgroup))
   post_file.close()
 
-archive_links_count = len(archive_links)
 archive_header = index_header
-archive_header = string.replace(archive_header, "page_title", "Archive - " + Settings.site_name)
-archive_header = string.replace(archive_header, "css/style.css", "../css/style.css")
+archive_header = archive_header.replace("page_title", "Archive - " + Settings.site_name)
+archive_header = archive_header.replace("css/style.css", "../css/style.css")
 
 archive_body = ""
-for link in archive_links:
-  archive_body = archive_body + "#" + str(archive_links_count) + " - " + link
-  archive_links_count -= 1
+for i in range(len(parsed_posts)):
+  archive_body = archive_body + "#" + str(i) + " - " +"<span>" + parsed_posts[i].date + "</span><a href='" + parsed_posts[i].url + "'>" + parsed_posts[i].title + "</a><br>"
 
-archive_file.write(archive_header + archive_body + string.replace(footer,"<!-- recent posts -->",postgroup))
+archive_file = codecs.open("site/archive/index.html", mode="w", encoding="utf8")
+archive_file.write(archive_header + archive_body + footer.replace("<!-- recent posts -->",postgroup))
 archive_file.close()
 
-rss(posts, rss_file)
+rss(parsed_posts)
--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 class Settings:
+	# Change these values to match your site
 	site_name = "blogpy"
-	site_root = "http://blogpy.reddino.org/"
+	site_root = "http://example.com/"
 	site_description = "A simple static blog generator in python"
 	number_of_posts_on_front_page = 5
